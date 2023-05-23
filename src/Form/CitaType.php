7203ede8ba3b290e18e4a8b0c79d44424d6efa22
Jaime Corpas <jaime.corpas.785@estudiante.iesoretania.es>
2023-05-23 13:30:12 +0200

    Solucionado error en modificación de citas

--- src/Form/CitaType.php
+++ src/Form/CitaType.php
@@ -4,6 +4,7 @@ namespace App\Form;
 
 use App\Entity\Cita;
 use Symfony\Component\Form\AbstractType;
+use Symfony\Component\Form\Extension\Core\Type\DateType;
 use Symfony\Component\Form\FormBuilderInterface;
 use Symfony\Component\OptionsResolver\OptionsResolver;
 
@@ -12,7 +13,10 @@ class CitaType extends AbstractType
     public function buildForm(FormBuilderInterface $builder, array $options)
     {
         $builder
-            ->add('fecha')
+            ->add('fecha', DateType::class, [
+                'label' => 'Fecha de nacimiento',
+                'widget' => 'text'
+            ])
         ;
     }
     public function configureOptions(OptionsResolver $resolver)
